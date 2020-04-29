c78fcedafc5e90ec9b5915490bb644d3ea23c229
Sushant Keny <sushant.keny23@gmail.com>
2020-04-29 11:29:30 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,28 @@
+/**
+ * @module Index
+ * @description Group the data and export it to excel sheet
+ * @author KenySushant
+ */
+const fs = require('fs');
+const path = require('path');
+const parser = require('parse-json');
+const xlsx = require('xlsx');
+
+const format = require('./lib/format');
+
+async function main() {
+  const rawData = await fs.promises.readFile(path.join(__dirname, 'data.txt'), 'utf-8');
+  const organizations = parser(rawData);
+  const submissions = format.toOrganizationsToCalculateMonitorUsage(organizations);
+  console.log(submissions);
+
+  // Add data to spreadsheet
+  const workbook = xlsx.utils.book_new();
+  const worksheet = xlsx.utils.json_to_sheet(submissions);
+  xlsx.utils.book_append_sheet(workbook, worksheet, 'Submissions');
+
+  // Write to file
+  xlsx.writeFile(workbook, 'Monitors.xlsx');
+}
+
+main().then(() => { }).catch((error) => console.error(error));
